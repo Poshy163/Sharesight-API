43189a9a979986ad1cc9731a0187071fa1f79d64
Poshy163 <poshernater@outlook.com>
2024-07-03 01:07:33 +0930

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SharesightAPI",
+    version="1.0.0",
+    author="Joshua Leaper",
+    author_email="poshernater163@gmail.com",
+    description="A Pyhton libary to access your sharesight portfolio infomation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Poshy163/Sharesight-API",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
