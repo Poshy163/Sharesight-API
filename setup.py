f9eb6128090db5b7172adf8269c45542171fefa0
Joshua Leaper <poshernater@outlook.com>
2024-07-17 21:29:04 +0930

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="SharesightAPI",
-    version="1.1.4",
+    version="1.1.5",
     author="Joshua Leaper",
     author_email="poshernater163@gmail.com",
     description="A Python library to access your sharesight portfolio information",
